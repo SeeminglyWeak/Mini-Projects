6638d720cbd8840c5f04c2f2dae6b0aa7fec1d83
Tanmay Verma <2006tanmay13@gmail.com>
2025-09-23 01:18:50 +0530

    Expanded the manager handle and created a new common class to be used by both customer and manager.

--- Inventory_Management.java
+++ Inventory_Management.java
@@ -8,8 +8,17 @@ Simple Inventory Management :
 */
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.LinkedList;
 import java.util.Scanner;
+
+class Common_Handle{
+    void listProducts(HashMap<String,Integer> ID, LinkedList<Integer> price, LinkedList<Integer> stock){
+        for(Map.Entry<String,Integer> v : ID.entrySet()){
+            System.out.println("Product Name-> " + v.getKey() + "Product Price->₹" + price.get(v.getValue()) + "Stock Available->" + stock.get(v.getValue()));
+        }
+    }
+}
 
 class Customer_Handle{
     
@@ -28,7 +37,37 @@ class Manager_Handle{
         }
     }
 
+    void addingProducts(HashMap<String,Integer> ID,LinkedList<Integer> price,LinkedList<Integer> stock, Scanner in){
+        System.out.print("Enter the product name : ");
+        String prodName = in.nextLine();
+        System.out.print("What should be the prduct ID : ");
+        int ID_ = in.nextInt();
+        ID.put(prodName, ID_);
+        System.out.print("What is the price : ");
+        int amount = in.nextInt();
+        price.add(amount);
+        System.out.print("What is the stock : ");
+        int quantity= in.nextInt();
+        stock.add(quantity);
+    }
 
+    void changeStock(HashMap<String,Integer> ID, LinkedList<Integer> stock, Scanner in){
+        System.out.print("Which product's stock is be altered : ");
+        String product = in.nextLine();
+        int idx = ID.get(product);
+        System.out.print("What is the updated stock : ");
+        int updated_stock = in.nextInt();
+        stock.add(idx, updated_stock);
+    }
+
+    void changePrice(HashMap<String,Integer> ID, LinkedList<Integer> price, Scanner in){
+        System.out.print("Which product's price is be altered?\n");
+        String product = in.nextLine();
+        int idx = ID.get(product);
+        System.out.print("What is the updated price : ");
+        int updated_price = in.nextInt();
+        price.add(idx, updated_price);
+    }
 }
 
 public class Inventory_Management {
@@ -37,7 +76,7 @@ public class Inventory_Management {
         Scanner in = new Scanner(System.in);
 
         //Data Structures
-        HashMap <Integer,String> products = new HashMap<>(); //Storing Product ID and Product Name
+        HashMap <String,Integer> products = new HashMap<>(); //Storing Product ID and Product Name
         LinkedList <Integer> stock = new LinkedList<>(); //Storing Product Price
         LinkedList <Integer> price = new LinkedList<>(); //Storing Product Quantity
 
@@ -67,8 +106,11 @@ public class Inventory_Management {
             case "Customer":
             case "customer":
 
+                break;
+
             case "Manager":
             case "manager":
+                Manager_Handle obj_1 = new Manager_Handle();
                 
                 if(Manager_Handle.login(in)){
                 System.out.println("Access Granted!");
@@ -77,9 +119,26 @@ public class Inventory_Management {
                 break;
                 }
 
-                System.out.println("What would you like to do?\n1.");
-            default:
-
+                System.out.println("What would you like to do?\n1.Add Products\n2.Alter Stocks\n3.Alter Price");
+                String action = in.nextLine();
+                switch (action) {
+                    case "1":
+                    case "Add Products":
+                        obj_1.addingProducts(products, price, stock, in);
+                        break;
+                    case "2":
+                    case "Alter Stocks":
+                        obj_1.changeStock(products, stock, in);
+                        break;
+                    case "3":
+                    case "Alter Price":
+                        obj_1.changePrice(products, price, in);
+                        break;
+                
+                    default:
+                        System.out.println("Enter a valid command!!");
+                        break;
+                }
             }
             
             System.out.print("Do you want to use inventory management again?\n1.Yes\n2.No\n->");
